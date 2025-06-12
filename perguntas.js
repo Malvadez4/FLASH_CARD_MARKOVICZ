2c57fe0e6a35ba68876c5742a73641a55620a689
Malvadez4 <vinicius.markovicz.souza@escola.pr.gov.br>
2025-06-12 19:22:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'SKINS',
+    ' O que são skins em Fortnite?',
+    'Skins são aparências personalizadas para os personagens, que não afetam a jogabilidade, mas permitem que os jogadores se destaquem visualmente.'
+)
+
+criaCartao(
+    'CONSTRUÇÃO',
+    'O que é uma construção no Fortnite e como ela funciona?',
+    'Construções são estruturas como paredes, rampas e pisos que os jogadores podem criar usando materiais coletados no jogo, oferecendo vantagem estratégica durante as batalhas.'
+)
+
+criaCartao(
+    'MOEDA',
+    'O que são V-Bucks?',
+    'V-Bucks são a moeda virtual do Fortnite usada para comprar itens cosméticos como skins, danças, envelopamentos e o Passe de Batalha.'
+)
+
+criaCartao(
+    'EVENTOS',
+    'Fortnite tem eventos ao vivo?',
+    'Sim, Fortnite é conhecido por seus eventos ao vivo, como shows de artistas, colaborações com filmes e mudanças drásticas no mapa em tempo real.'
+)
